e670d68c2ac5036923d27c2f692f63187db4d54b
lizyaaa <lizyaaa@dcits.com>
2022-09-21 16:13:36 +0800

    加入动画 loadingCircle

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,4 @@ onMounted(() => {
   padding: 1.5em;
   will-change: filter;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
